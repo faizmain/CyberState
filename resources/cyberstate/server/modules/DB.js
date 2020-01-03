c6e7d5efa0742419bda824d50fe6eca94684e8cf
faizmain <egor.dust2@outlook.com>
2020-01-03 22:26:12 +0300

    loaded base

---
+++ resources/cyberstate/server/modules/DB.js
@@ -0,0 +1,29 @@
+var mysql = require('mysql');
+
+module.exports = {
+    Handle: null,
+    Connect: function(callback) {
+        this.Handle = mysql.createPool({ 
+            connectionLimit: 10,
+            host: 'localhost',
+            user: 'root',
+            password: 'qweqwe123',
+            database: 'gamemode',
+            debug: false
+        });
+        callback();
+    },
+    Query: function(text, params, callback) {
+        DB.Handle.query(text, params, (err, result) => {
+            if (typeof callback == "function") callback(err, result);
+        });
+    },
+    Characters: {
+        getSqlIdByName: (name, callback) => {
+            DB.Query("SELECT id FROM characters WHERE name=?", [name], (err, result) => {
+                if (result.length > 0) callback(result[0].id);
+                else callback(0);
+            });
+        }
+    }
+};
